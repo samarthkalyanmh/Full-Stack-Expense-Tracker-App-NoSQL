11bf2af0596b678a756626ffdae681ac80cd6b94
samarthkalyanmh <samarthkalyanmh777@gmail.com>
2023-02-25 02:20:07 +0530

    Optimized getting user expense details in premium-controller using SQL joins

--- Controllers/premium-controller.js
+++ Controllers/premium-controller.js
@@ -1,30 +1,19 @@
 const User = require('../Models/user-model')
 const Expense = require('../Models/expense-model')
+const sequelize = require('../util/database')
 
 const showLeaderBoard = async (req, res, next) => {
-    try{    
-            const users = await User.findAll()
-            const expenses = await Expense.findAll()
-            const userMatchedExpenses = {}
-            
-            expenses.forEach((expense) => {
-
-                if(userMatchedExpenses[expense.UserId]){
-                    userMatchedExpenses[expense.UserId] = userMatchedExpenses[expense.UserId] + expense.amount
-                } else{
-                    userMatchedExpenses[expense.UserId] = expense.amount
-                }
-            })
-
-            let userLeaderBoardDetails = []
-            users.forEach(user => {
-                userLeaderBoardDetails.push({name : user.name, totalExpense: userMatchedExpenses[user.id]})
-            })
-
-            console.log(userLeaderBoardDetails)
-
-            userLeaderBoardDetails.sort((a, b) => {
-                return  b.totalExpense - a.totalExpense 
+    try{   
+            const userLeaderBoardDetails = await User.findAll({
+                attributes: ['id', 'name',[sequelize.fn('sum', sequelize.col('expenses.amount')), 'totalExpense'] ],
+                include: [
+                    {
+                        model: Expense,
+                        attributes: []
+                    }
+                ],
+                group:['user.id'],
+                order:[['totalExpense', 'DESC']]
             })
 
             res.status(200).json(userLeaderBoardDetails)
