43cc92e422b252915993150a35a81306937f784d
samarthkalyanmh <samarthkalyanmh777@gmail.com>
2023-03-30 14:21:16 +0530

    Changed purchase controller code to mongoose

--- Controllers/purchase-controller.js
+++ Controllers/purchase-controller.js
@@ -1,7 +1,7 @@
 const Razorpay = require('razorpay')
 const Order = require('../Models/order-model')
 const userController = require('./user-controller')
-const sequelize = require('../util/database')
+const User = require('../Models/user-model')
 require('dotenv').config()
 
 
@@ -14,17 +14,19 @@ const purchasePremium = async (req, res, next) => {
         })
 
 
-        const amount = 45000
+        const amount = 4500
         
         //Creating order in razorpay
-        rzp.orders.create({amount, currency: 'INR'}, (err, order) => {
+        rzp.orders.create({amount, currency: 'INR'}, async (err, order) => {
             if(err){
                 // throw new Error(JSON.stringify(err))
-                return res.status(500).json(err, {message: 'something went wrong in razorpay order creation process'}) 
+                return res.status(500).json({message: 'something went wrong in razorpay order creation process', err: err}) 
             }
 
             //saving order details in DataBase
-            req.user.createOrder({orderid: order.id, status:'PENDING'}) 
+            //req.user.createOrder({orderid: order.id, status:'PENDING'}) 
+            Order.create({orderid: order.id, status:'PENDING'})
+
             .then(() => {
                 return res.status(201).json({order, key_id: rzp.key_id})
             })
@@ -42,62 +44,73 @@ const purchasePremium = async (req, res, next) => {
 
 const updateTransactionStatus = async (req, res, next) => {
 
-    const t = await sequelize.transaction()
+    // const t = await sequelize.transaction()
     try {
         
-        const { payment_id, order_id} = req.body;
-        const order  = await Order.findOne({where : {orderid : order_id}}) 
-        const promise1 =  order.update({ paymentid: payment_id, status: 'SUCCESSFUL'}, { transaction: t }) 
-        const promise2 =  req.user.update({ isPremiumUser: true }, { transaction: t })
+        const { payment_id, order_id} = req.body
+
+        //const order  = await Order.findOne({orderid : order_id}) 
+
+        // const promise1 =  order.update({ paymentid: payment_id, status: 'SUCCESSFUL'})
+        // const promise2 =  req.user.update({ isPremiumUser: true })
+
+        const promise1 = Order.updateOne({orderid: order_id}, {paymentid: payment_id, status: 'SUCCESSFUL'})
+        const promise2 = User.updateOne({_id: req.user._id}, {isPremiumUser: true})
+
 
         //These below declarations must be made here itself to get the updated isPremiumUser status, if we make it above we will get old value which was null
-        const userId = req.user.id
+        const userId = req.user._id
         const name = req.user.name
         const isPremiumUser = req.user.isPremiumUser 
 
         Promise.all([promise1, promise2]).then(async ()=> {
-            await t.commit()
+            // await t.commit()
             return res.status(202).json({sucess: true, message: "Transaction Successful", token: userController.generateAccessToken(userId, name, isPremiumUser) }) 
         }).catch(async (error ) => {
-            await t.rollback()
+            // await t.rollback()
             throw new Error(error)
         })   
                 
     } catch (err) {
-        await t.rollback()
+        // await t.rollback()
 
         console.log('Error in updateTransactionStatus', err)
-        res.status(500).json({message: 'Internal Server Error 500', err})
+        res.status(500).json({message: 'Internal Server Error 500', err: err})
     }
 }
 
 //Not working yet
 const updateTransactionStatusFailed = async (req, res, next) => {
 
-    const t = await sequelize.transaction()
+    // const t = await sequelize.transaction()
 
     try{
-        const userId = req.user.id
+        const userId = req.user._id
         const name = req.user.name
 
         console.log('transaction failed>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
         const { order_id } = req.body
-        const order  = await Order.findOne({where : {orderid : order_id}}) 
+        // const order  = await Order.findOne({where : {orderid : order_id}}) 
 
-        const promise1 =  order.update({ paymentid: 'FAILED', status: 'FAILED'}, { transaction: t }) 
-        const promise2 =  req.user.update({ isPremiumUser: false }, { transaction: t }) 
 
-        Promise.all([promise1, promise2]).then(async ()=> {
-            await t.commit()
+
+        /* const promise1 =  order.update({ paymentid: 'FAILED', status: 'FAILED'}, { transaction: t }) 
+        const promise2 =  req.user.update({ isPremiumUser: false }, { transaction: t })  */
+
+        const promise1 = Order.updateOne({orderid: order_id}, {paymentid: 'FAILED', status: 'FAILED'})
+        // const promise2 = User.updateOne()
+
+        Promise.all([promise1]).then(async ()=> {
+            // await t.commit()
             return res.status(403).json({result: false, message: "Transaction Failed machi", token: userController.generateAccessToken(userId, name) })
         }).catch(async (error ) => {
-            await t.rollback()
+            // await t.rollback()   
             throw new Error(error)
         }) 
 
     } catch(err){
-        await t.rollback()
+        // await t.rollback()
         console.log('Error in updateTransactionStatusFailed', err)
         res.status(500).json({ error: err, message: 'Something went wrong'})
     }
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+//What does this below code do exactly?
 // app.use(express.static('public'))
 require('dotenv').config()
 const path = require('path')
@@ -9,15 +11,6 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const fs = require('fs')
 // const helmet = require('helmet')
-
-/* MODELS */
-// const User = require('./Models/user-model')
-// const Expense = require('./Models/expense-model')
-// const Order = require('./Models/order-model')
-// const ForgotPassword = require('./Models/forgot-password-model')
-// const FileURL = require('./Models/previous-downloads-fileURL-model')
-
-// const sequelize = require('./util/database')
 
 const cors = require('cors')
 
@@ -33,36 +26,37 @@ app.use(morgan('combined', {stream: accessLogStream}))
 
 app.use(bodyParser.json({extended:false}))
 
-
-/* ROUTES */
 const signupRoute = require('./Routes/signup-route')
 const loginroute = require('./Routes/login-route')
 const expenseRoute = require('./Routes/expense-route')
 const passwordRoute = require('./Routes/password-route')
 const premiumRoute = require('./Routes/premium-route')
-// const purchaseRoute = require('./Routes/purchase-route')
+const purchaseRoute = require('./Routes/purchase-route')
 
 app.use(signupRoute)
 app.use(loginroute)
 app.use(expenseRoute)
 app.use(passwordRoute) 
 app.use(premiumRoute)
-// app.use(purchaseRoute)
-  
-
+app.use(purchaseRoute)
 
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, `public/${req.url}`))
 });
 
+// (function(){ 
 
-(function(){ 
-    
-    console.log('inside')
-    mongoose.connect(process.env.MONGODB_ATLAS_URL)
+//     mongoose.connect(process.env.MONGODB_ATLAS_URL)
+//     .then(() => {
+//         console.log('Connected to atlas!')
+//         app.listen(3000)
+//     })
+//     .catch(err => console.log(err))
+// })()
+
+mongoose.connect(process.env.MONGODB_ATLAS_URL)
     .then(() => {
         console.log('Connected to atlas!')
         app.listen(3000)
     })
     .catch(err => console.log(err))
-})()
